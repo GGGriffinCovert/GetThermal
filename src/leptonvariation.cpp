5426e215a8b89944d265ffdcdc753054cc8109af
Griffin <griffin@groupgets.com>
2025-03-26 14:48:00 -0700

    Update leptonvariation.cpp
    Work with any "500-0758-XX" part number for Lepton 3.1R

--- src/leptonvariation.cpp
+++ src/leptonvariation.cpp
@@ -138,7 +138,8 @@ bool LeptonVariation::getSupportsRadiometry()
     bool runtimeAgc = getSupportsRuntimeAgcChange();
     bool y16Firmware = getPtFirmwareVersion().contains("Y16");
     bool radiometricLepton = getOemFlirPartNumber().contains("500-0763-01")
-            || getOemFlirPartNumber().contains("500-0771-01") || getOemFlirPartNumber().contains("500-0758-01") ;
+            || getOemFlirPartNumber().contains("500-0771-01") 
+            || getOemFlirPartNumber().contains("500-0758");
     return (runtimeAgc || y16Firmware) && radiometricLepton;
 }
 
